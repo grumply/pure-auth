4461f5f1551be0d3bbaf6aa37e72b1479ffbb62e
Sean Hickman <seanmhickman@gmail.com>
2021-09-27 23:44:04 -0500

    Add a sorcerer listener db helper.

--- src/Pure/Auth/GHC.hs
+++ src/Pure/Auth/GHC.hs
@@ -1,4 +1,12 @@
-module Pure.Auth.GHC (module Export) where
+{-# language AllowAmbiguousTypes, TypeApplications, ScopedTypeVariables #-}
+module Pure.Auth.GHC (module Export,authDB) where
 
 import Pure.Auth.GHC.API as Export
 import Pure.Auth.GHC.Auth as Export (AuthEvent(Deleted),Stream(AuthEventStream),Auth)
+
+import Pure.Sorcerer (Listener,listener)
+
+import Data.Typeable
+
+authDB :: forall _role. Typeable _role => [Listener]
+authDB = [listener @(AuthEvent _role) @(Auth _role) ]
